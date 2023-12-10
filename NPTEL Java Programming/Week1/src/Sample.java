bd9f78b87fabbbc53053f15a88d412a94f9cf77c
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2023-12-10 22:05:01 +0530

    Add files via upload

---
+++ NPTEL Java Programming/Week1/src/Sample.java
@@ -0,0 +1,8 @@
+
+public class Sample  {
+    public static void main(String[] args) {
+        char nptel[] = {'S','W','Y','A','M'};
+        System.out.println(""+nptel[1]+nptel[nptel.length-1]);
+    }
+
+}

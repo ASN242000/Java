e69386316c21dd0f225ef98ea83dcbe9086fdab9
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-01-17 22:18:19 +0530

    Add files via upload

--- Misellaneous/src/InsertionSort.java
+++ Misellaneous/src/InsertionSort.java
@@ -1,22 +1,21 @@
 public class InsertionSort {
     public static void main(String[] args) {
-        int a[] = {7,4,1,2,9,0,3,8};
-        insertSort(a);
-    }
-    static void insertSort(int a[]){
-        int j,key;
-
-        for(int i=1;i<a.length;i++){
-            key = a[i];
-            j=i-1;
-            while( j>=0 && a[j]>key){
-                a[j+1] = a[j];
-                j=j-1;
-            }
-            a[j+1] = key;
-        }
+        int a[] = {3,2,5,1,6,8,4};
+        insertionSort(a);
         for(int i=0;i<a.length;i++){
             System.out.print(a[i]+" ");
         }
     }
+    public static void insertionSort(int a[]){
+        for(int i=1;i<a.length;i++){
+            int j=i-1;
+            int x = a[i];
+            while(j>=0 && a[j]>x){
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1] = x;
+        }
+    }
+
 }

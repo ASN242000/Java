18c0936b8b5566f6aa9fa0241fef66f7af476301
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-01-17 22:17:32 +0530

    Add files via upload

--- Misellaneous/src/BinarySearch.java
+++ Misellaneous/src/BinarySearch.java
@@ -1,25 +1,39 @@
+import java.util.Scanner;
+
 public class BinarySearch {
     public static void main(String[] args) {
-        int a[] = {2,5,7,8,9};
-        binarySearch(a, 9);
-    }
-    static void binarySearch(int a[],int target){
-        int low=0, high=a.length-1, mid;
-        mid=(low+high)/2;
-        while(low<=high){
-            if(target == a[mid])
-            {
-                System.out.println("Element found at location "+mid);
-                break;
-            }
-            else if(target<a[mid])
-                high=mid-1;
-            else
-                low=mid+1;
-            mid=(low+high)/2;
+        Scanner rd= new Scanner(System.in);
+        int n, a[];
+        System.out.println("Enter the number of elements of the array");
+        n = rd.nextInt();
+        a = new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+            a[i] = rd.nextInt();
         }
-        if(low>high)
+        int target;
+        System.out.println("Enter the element to be searched");
+        target = rd.nextInt();
+        BinarySearch b = new BinarySearch();
+        if(b.BSearch(a,target) == -1)
             System.out.println("Element not found");
+        else
+            System.out.println("Element found at "+(b.BSearch(a,target)+1)+" position");
     }
-
+    public int BSearch(int q[],int target){
+        int low=0,high=q.length,mid;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(q[mid] == target){
+                return mid;
+            }
+            if(q[mid]<target){
+                low = mid+1;
+            }
+            if(q[mid]>target){
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
 }

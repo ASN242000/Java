5338de3e55e4b165ff9e2ad93790d75bb582a97a
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-01-18 20:14:00 +0530

    Add files via upload

---
+++ StringHandling/src/ContentEquals.java
@@ -0,0 +1,7 @@
+public class ContentEquals {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        System.out.println(s1.contentEquals(s2));
+    }
+}

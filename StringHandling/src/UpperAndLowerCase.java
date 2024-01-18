cdbb193ac980b3fc6db0a6b3e734088d01027562
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-01-18 20:12:52 +0530

    Add files via upload

---
+++ StringHandling/src/UpperAndLowerCase.java
@@ -0,0 +1,9 @@
+public class UpperAndLowerCase {
+    public static void main(String[] args) {
+        String s ="Hello";
+
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+
+    }
+}

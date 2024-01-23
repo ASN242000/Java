3c8ae820d1bd60873d6729d18998c50b06ceebf2
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-01-23 21:40:31 +0530

    Add files via upload

---
+++ StringHandling/src/StringBufferInsert.java
@@ -0,0 +1,7 @@
+public class StringBufferInsert {
+    public static void main(String[] args) {
+        StringBuffer a = new StringBuffer("I Java!");
+        a.insert(1," love");
+        System.out.println(a);
+    }
+}

ab3e1895b3ce1586b2d9e1bcc3b891f5e6780c21
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-01-23 21:40:05 +0530

    Add files via upload

---
+++ StringHandling/src/StringBufferIndexOf.java
@@ -0,0 +1,8 @@
+public class StringBufferIndexOf {
+    public static void main(String[] args) {
+        StringBuffer s = new StringBuffer("Hey! How are you?");
+        System.out.println(s.indexOf("H"));
+        System.out.println(s.lastIndexOf("H"));
+
+    }
+}

7251dbcad31eba065805f63d863d51bddd95abe7
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2024-01-23 21:38:47 +0530

    Delete StringHandling/SubStringS.java

--- StringHandling/SubStringS.java
+++
@@ -1,7 +0,0 @@
-public class SubStringS {
-    public static void main(String[] args) {
-        String s = "Hello World!";
-        System.out.println(s.substring(6));
-        System.out.println(s.substring(1,11));
-    }
-}

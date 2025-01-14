752bf8ce1ec8a5c751d76e03b4b93dc768a04e9a
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2025-01-14 08:49:01 +0530

    .

--- Two_Overview/src/Example2_Ch2.java
+++
@@ -1,9 +0,0 @@
-public class Example2_Ch2 {
-    public static void main(String[] args) {
-        int num;
-        num = 100;
-        System.out.println("The value of num " + num);
-        num = num * 5;
-        System.out.println("The value of num " + num);
-    }
-}
--- Two_Overview/src/Example_Ch2.java
+++
@@ -1,5 +0,0 @@
-public class Example_Ch2 {
-    public static void main(String[] args) {
-        System.out.println("This is a simple Java program");
-    }
-}

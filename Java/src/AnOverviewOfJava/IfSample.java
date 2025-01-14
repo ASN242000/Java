b5d62ec4749a0cdb0852c0fe719a219c7cec22ff
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2025-01-14 08:48:32 +0530

    Java Overview

---
+++ Java/src/AnOverviewOfJava/IfSample.java
@@ -0,0 +1,14 @@
+package AnOverviewOfJava;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x,y;
+        x=10;
+        y=20;
+        if (x < y) System.out.println("x is less then y");
+        x=x*2;
+        if(x == y) System.out.println("x is now equal to y");
+        x= x+10;
+        if(x > y) System.out.println("x is now greater than y");
+    }
+}

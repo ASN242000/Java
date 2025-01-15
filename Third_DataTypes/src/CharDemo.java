7c962a8e349b9bec56ec869ff178edaed0d87a4a
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2025-01-15 16:35:23 +0530

    Delete

--- Third_DataTypes/src/CharDemo.java
+++
@@ -1,8 +0,0 @@
-public class CharDemo {
-    public static void main(String[] args) {
-        char ch1,ch2;
-        ch1=88;
-        ch2='Y';
-        System.out.println("Ch1 and ch2 are "+ch1+" "+ch2);
-    }
-}
--- Third_DataTypes/src/CharDemo2.java
+++
@@ -1,9 +0,0 @@
-public class CharDemo2 {
-    public static void main(String[] args) {
-        char ch1;
-        ch1='X';
-        System.out.println("ch1 value is "+ch1);
-        ch1++;
-        System.out.println("ch1++ value is "+ch1);
-    }
-}

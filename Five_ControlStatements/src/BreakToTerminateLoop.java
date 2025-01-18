2256ddec8e16b8c878a2fcc66595eae445ca01d6
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2025-01-18 22:06:19 +0530

    Delete Five_ControlStatements/src/BreakToTerminateLoop.java

--- Five_ControlStatements/src/BreakToTerminateLoop.java
+++
@@ -1,9 +0,0 @@
-public class BreakToTerminateLoop {
-    public static void main(String[] args) {
-        for(int i=0;i<100;i++){
-            if(i==10)
-                break;
-            System.out.println("Tick: "+(i+1));
-        }
-    }
-}

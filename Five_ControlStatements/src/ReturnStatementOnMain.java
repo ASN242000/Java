595778d0df5ec86ed2e45f5817a20e13a69825e8
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2025-01-23 21:30:43 +0530

    Delete Five_ControlStatements/src/ReturnStatementOnMain.java

--- Five_ControlStatements/src/ReturnStatementOnMain.java
+++
@@ -1,9 +0,0 @@
-public class ReturnStatementOnMain {
-    public static void main(String[] args) {
-        boolean t = true;
-        System.out.println("Before Return");
-        if(t)
-            return; // return to caller
-        System.out.println("This won't execute");
-    }
-}

9b0a857192d3049d479ec5dd7452e65c7ba71b9c
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2025-02-05 04:48:51 +0530

    Delete Inheritance/src/AbstractClassesMain.java

--- Inheritance/src/AbstractClassesMain.java
+++
@@ -1,7 +0,0 @@
-public class AbstractClassesMain {
-    public static void main(String[] args) {
-        AbstractClassesB b = new AbstractClassesB();
-        b.callme();
-        b.callmetoo();
-    }
-}

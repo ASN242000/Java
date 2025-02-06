92f7961c1dfd996a434baa59eb1d89fba83a5ef7
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2025-02-06 20:49:36 +0530

    Delete Inheritance/src/B.java

--- Inheritance/src/B.java
+++
@@ -1,9 +0,0 @@
-public class B extends A{
-    int k;
-    void showk(){
-        System.out.println("The value of k is "+k);
-    }
-    void sum(){
-        System.out.println("i+j+k is "+(i+j+k));
-    }
-}
